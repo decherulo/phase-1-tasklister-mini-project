f7cce4acf2f630a84759f8d2c6f90b2aaa1d04a6
Deborah Kimtai <decherulo@gmail.com>
2026-05-03 00:23:55 +0300

    complete tasklister lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.getElementById('create-task-form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const task = event.target['new-task-description'].value;
+  buildToDo(task);
+});
+function buildToDo(task) {
+  const li = document.createElement('li');
+  li.textContent = task;
+  document.getElementById('task').appendChild(li);
+}
 });
