31752c17915b0cf3341a4514f21ea100505a8c23
Deborah Kimtai <decherulo@gmail.com>
2026-05-03 00:43:29 +0300

    fix task input selector

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 const form = document.getElementById('create-task-form');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const task = event.target['new-task-description'].value;
+  const task = event.target.querySelector('[name="new-task-description"]').value;
   buildToDo(task);
 });
 function buildToDo(task) {
